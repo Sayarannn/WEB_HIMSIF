9129f98e05820742efa5a09fa934101dcc5243f3
Sayarannn <ap382025@gmail.com>
2023-01-26 14:02:33 +0700

    first commit

---
+++ application/views/administrator/footer.php
@@ -0,0 +1,2 @@
+
+<strong>Copyright &copy; 2023 - <?php echo date('Y'); ?> <a target='_BLANK' href=""> Himpunan Mahasiswa Sistem Informasi</a>.</strong> All rights reserved. 
---
+++ application/views/phpmu-sekolah/footer.php
@@ -0,0 +1,7 @@
+
+<?php 
+echo "<div class='wrapper' style='padding-top:0px;'><br>
+	<center>HIMSIF BINA DARMA © ".date('Y')." All Right Reserved</center>
+</div>";
+
+
